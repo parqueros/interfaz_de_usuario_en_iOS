6f913e973d1bd9c2f9d8f81aa4ca134ae4b87a1e
pabloarqueros <pearqueros@gmail.com>
2015-11-15 22:28:25 +0100

    Initial Commit

---
+++ PizzaApp_Semana2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaApp_Semana2
+//
+//  Created by Pablo Arqueros on 15/11/15.
+//  Copyright © 2015 Pablo Arqueros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
